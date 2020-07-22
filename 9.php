f59e8b819607adefb51065c0cd26a8542ea1d5cc
Colocust <516100080@qq.com>
2020-07-22 14:20:29 +0800

    commit 9

---
+++ 9.php
@@ -0,0 +1,44 @@
+<?php
+
+class CQueue {
+  private $stack;
+  private $popStack;
+
+  /**
+   */
+  function __construct() {
+    $this->stack = [];
+    $this->popStack = [];
+  }
+
+  /**
+   * @param Integer $value
+   * @return NULL
+   */
+  function appendTail($value) {
+    $this->stack[] = $value;
+    return null;
+  }
+
+  /**
+   * @return Integer
+   */
+  function deleteHead() {
+    if (empty($this->popStack)) {
+      while (!empty($this->stack)) {
+        $this->popStack[] = array_pop($this->stack);
+      }
+    }
+    if(empty($this->popStack)) {
+      return -1;
+    }
+    return array_pop($this->popStack);
+  }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * $obj = CQueue();
+ * $obj->appendTail($value);
+ * $ret_2 = $obj->deleteHead();
+ */
