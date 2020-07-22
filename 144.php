057142ebcd7008aaca3d3b246d978b829cab1388
Colocust <516100080@qq.com>
2020-07-22 21:33:10 +0800

    commit 144

---
+++ 144.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param TreeNode $root
+   * @return Integer[]
+   */
+  function preorderTraversal($root) {
+    if(!$root) {
+      return [];
+    }
+
+    $stack = [];
+    $stack[] = $root->val;
+
+    $left = $this->preorderTraversal($root->left);
+    foreach($left as $lV) {
+      $stack[] = $lV;
+    }
+
+    $right = $this->preorderTraversal($root->right);
+    foreach($right as $rV) {
+      $stack[] = $rV;
+    }
+
+    return $stack;
+  }
+}
