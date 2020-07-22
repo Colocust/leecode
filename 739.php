df00ab8804169953c662d6aec9bf6d9cfff43274
Colocust <516100080@qq.com>
2020-07-22 22:04:51 +0800

    commit 739

---
+++ 739.php
@@ -0,0 +1,26 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $T
+   * @return Integer[]
+   */
+  function dailyTemperatures($T) {
+    if (count($T) == 0) {
+      return [];
+    }
+    $stack = new SplStack();
+
+    $ret = [];
+    foreach ($T as $day => $t) {
+      $ret[$day] = 0;
+      while (!$stack->isEmpty() && $T[$stack->top()] < $t) {
+        $pop = $stack->pop();
+        $ret[$pop] = $day - $pop;
+      }
+      $stack->push($day);
+    }
+    return $ret;
+  }
+}
