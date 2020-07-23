b2591f1c6f53897b99b68d7b96175223c9af1f7d
Colocust <516100080@qq.com>
2020-07-23 20:28:41 +0800

    commit 844

---
+++ 844.php
@@ -0,0 +1,35 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String $S
+   * @param String $T
+   * @return Boolean
+   */
+  function backspaceCompare($S, $T) {
+    $sStack = [];
+    $tStack = [];
+
+    for ($s = 0; $s < strlen($S); $s++) {
+      if ($S[$s] == '#') {
+        if ($sStack) {
+          array_pop($sStack);
+        }
+      } else {
+        $sStack[] = $S[$s];
+      }
+    }
+
+    for ($t = 0; $t < strlen($T); $t++) {
+      if ($T[$t] == '#') {
+        if ($tStack) {
+          array_pop($tStack);
+        }
+      } else {
+        $tStack[] = $T[$t];
+      }
+    }
+    return $sStack == $tStack;
+  }
+}
