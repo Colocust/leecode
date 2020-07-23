7944b5bf51364a9bb5058641f2ac16c18305f640
Colocust <516100080@qq.com>
2020-07-23 19:43:06 +0800

    commit 1019

---
+++ 1019.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return Integer[]
+   */
+  function nextLargerNodes($head) {
+    $value = [];
+    while ($head) {
+      $value[] = $head->val;
+      $head = $head->next;
+    }
+    $result = [];
+    $stack = [];
+
+    foreach ($value as $key => $item) {
+      $result[$key] = 0;
+      while ($stack && $value[end($stack)] < $item) {
+        $result[array_pop($stack)] = $item;
+      }
+      $stack[] = $key;
+    }
+    ksort($result);
+    return $result;
+  }
+}
