ce791ac2c4fd1db4a2b4b9bc88332babe7bb1dd9
Colocust <516100080@qq.com>
2020-07-23 19:38:52 +0800

    commit 1249

---
+++ 1249.php
@@ -0,0 +1,34 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String $s
+   * @return String
+   */
+  function minRemoveToMakeValid($s) {
+    $arr = str_split($s);
+    $stack = [];
+
+    foreach ($arr as $key => $value) {
+      if ($value == '(') {
+        $stack[] = $key;
+        continue;
+      }
+      if ($value == ')') {
+        if (empty($stack) || $arr[end($stack)] == ')') {
+          $stack[] = $key;
+        } else {
+          array_pop($stack);
+        }
+      }
+    }
+    foreach ($stack as $unset) {
+      unset($arr[$unset]);
+    }
+    return implode('', $arr);
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->minRemoveToMakeValid("lee(t(c)o)de)"));
