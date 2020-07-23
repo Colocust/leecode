d02cedb9a026805e5cdbfbacb8b7bd55f76694f6
Colocust <516100080@qq.com>
2020-07-23 14:06:11 +0800

    commit 946

---
+++ 946.php
@@ -0,0 +1,22 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $pushed
+   * @param Integer[] $popped
+   * @return Boolean
+   */
+  function validateStackSequences($pushed, $popped) {
+    $stack = [];
+    $i = 0;
+    foreach ($pushed as $e) {
+      array_push($stack, $e);
+      while ($stack && end($stack) == $popped[$i]) {
+        array_pop($stack);
+        $i++;
+      }
+    }
+    return $stack ? false : true;
+  }
+}
