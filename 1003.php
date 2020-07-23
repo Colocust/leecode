9903849bf13bb3cae15efeec69f7a708e743f8e4
Colocust <516100080@qq.com>
2020-07-23 14:25:54 +0800

    commit 1003

---
+++ 1003.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String $S
+   * @return Boolean
+   */
+  function isValid($S) {
+    $stack = new SplStack();
+    $length = strlen($S);
+    for ($i = 0; $i < $length; $i++) {
+      if ($S[$i] == 'b') {
+        if ($stack->isEmpty() || $stack->top() !== 'a') {
+          return false;
+        }
+        $stack->push('b');
+      } else if ($S[$i] == 'c') {
+        if ($stack->isEmpty() || $stack->top() !== 'b') {
+          return false;
+        }
+        $stack->pop();
+        $stack->pop();
+      } else {
+        $stack->push($S[$i]);
+      }
+    }
+    return $stack->isEmpty();
+  }
+}
