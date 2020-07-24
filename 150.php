c1846b1e43528d3b590817b52fa5f4b24626a0fc
Colocust <516100080@qq.com>
2020-07-24 17:31:16 +0800

    commit 150

---
+++ 150.php
@@ -0,0 +1,44 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param String[] $tokens
+   * @return Integer
+   */
+  function evalRPN($tokens) {
+    $stack = new SplStack();
+    for ($i = 0; $i < count($tokens); $i++) {
+      if (is_numeric($tokens[$i])) {
+        $stack->push($tokens[$i]);
+        continue;
+      }
+      $n1 = $stack->pop();
+      $n2 = $stack->pop();
+      switch ($tokens[$i]) {
+        case '+':
+          $stack[] = $n2 + $n1;
+          break;
+        case '-':
+          $stack[] = $n2 - $n1;
+          break;
+        case '*':
+          $stack[] = $n2 * $n1;
+          break;
+        case '/':
+          $stack[] = (int)($n2 / $n1);
+          break;
+        default:
+          break;
+      }
+    }
+    $sum = 0;
+    while (!$stack->isEmpty()) {
+      $sum += $stack->pop();
+    }
+    return $sum;
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]));
