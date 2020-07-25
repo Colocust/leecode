8094a46396cf63f7db9d714e1157438cf0bb3ec6
Colocust <516100080@qq.com>
2020-07-25 22:20:01 +0800

    commit 876

---
+++ 876.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return ListNode
+   */
+  function middleNode($head) {
+    if (!$head || !$head->next) {
+      return $head;
+    }
+    $fast = $head;
+    $slow = $head;
+    while ($fast && $fast->next) {
+      $fast = $fast->next->next;
+      $slow = $slow->next;
+    }
+    return $slow;
+  }
+}
