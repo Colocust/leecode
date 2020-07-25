074916cd9d091e4230a15b0f9bdf6e78e7ca2093
Colocust <516100080@qq.com>
2020-07-25 22:13:41 +0800

    commit 206

---
+++ 206.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return ListNode
+   */
+  function reverseList($head) {
+    if(!$head->next) {
+      return $head;
+    }
+    $node = $this->reverseList($head->next);
+    $next = $head->next;
+    $next->next = $head;
+    $head->next = null;
+
+    return $node;
+  }
+}
