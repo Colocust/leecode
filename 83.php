0db9b7bf63ce3169ec2a1638efb1fe7a91dd4bd2
Colocust <516100080@qq.com>
2020-07-25 22:41:21 +0800

    commit 83

---
+++ 83.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @return ListNode
+   */
+  function deleteDuplicates($head) {
+    $cur = $head;
+    while ($cur && $cur->next) {
+      if ($cur->val == $cur->next->val) {
+        $cur->next = $cur->next->next;
+      } else {
+        $cur = $cur->next;
+      }
+    }
+    return $head;
+  }
+}
