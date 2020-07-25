cd1fe713f2d76e25434e5501c76d2e19e9ecdb16
Colocust <516100080@qq.com>
2020-07-25 21:47:47 +0800

    commit 383

---
+++ 383.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @param Integer $k
+   * @return Integer
+   */
+  function kthToLast($head, $k) {
+    $fast = $head;
+    $slow = $head;
+    while($k>0) {
+      $fast = $fast->next;
+      $k--;
+    }
+    while($fast) {
+      $fast = $fast->next;
+      $slow = $slow->next;
+    }
+    return $slow->val;
+  }
+}
