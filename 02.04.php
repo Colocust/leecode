45960b8ee010f57132587c386b4184658391c484
Colocust <516100080@qq.com>
2020-07-27 21:24:52 +0800

    commit 02.04

---
+++ 02.04.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $head
+   * @param Integer $x
+   * @return ListNode
+   */
+  function partition($head, $x) {
+    if (!$head || !$head->next) {
+      return $head;
+    }
+
+    $cur = $head;
+    $p = $head;
+    while ($cur) {
+      if ($cur->val < $x) {
+        $temp = $p->val;
+        $p->val = $cur->val;
+        $cur->val = $temp;
+        $p = $p->next;
+      }
+      $cur = $cur->next;
+    }
+
+    return $head;
+  }
+}
