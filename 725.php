0b8a08c75cd35811949c7bddf11c21dd89dbcc5b
Colocust <516100080@qq.com>
2020-07-28 15:03:38 +0800

    commit 725

---
+++ 725.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+  /**
+   * @param ListNode $root
+   * @param Integer $k
+   * @return ListNode[]
+   */
+  function splitListToParts($root, $k) {
+    $length = 0;
+    $cur = $root;
+    while ($cur) {
+      $length++;
+      $cur = $cur->next;
+    }
+    $avg = (int)($length / $k);
+    $remind = (int)($length % $k);
+
+    $results = [];
+    for ($i = 0; $i < $k; $i++) {
+      $pre = null;
+      $results[] = $root;
+      $cycleTime = $remind ? $avg + 1 : $avg;
+
+      for ($j = 0; $j < $cycleTime; $j++) {
+        $pre = $root;
+        $root = $root->next;
+      }
+
+      if ($remind) $remind--;
+      if ($pre) $pre->next = null;
+    }
+    return $results;
+  }
+}
