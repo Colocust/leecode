82bd07d18afdddfb8c2b6f3c267ec82987468c65
Colocust <516100080@qq.com>
2020-08-01 20:48:13 +0800

    commit 75

---
+++ 75.php
@@ -0,0 +1,38 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @return NULL
+   */
+  function sortColors(&$nums) {
+    $zero = -1;
+    $two = count($nums);
+
+    for ($i = 0; $i < $two;) {
+      if ($nums[$i] == 1) {
+        $i++;
+      } else if ($nums[$i] == 2) {
+        $two--;
+        $this->swap($nums, $two, $i);
+      } else {
+        $zero++;
+        $this->swap($nums, $zero, $i);
+        $i++;
+      }
+    }
+    return null;
+  }
+
+  function swap(&$nums, $i, $j) {
+    $temp = $nums[$i];
+    $nums[$i] = $nums[$j];
+    $nums[$j] = $temp;
+  }
+}
+
+$solution = new Solution();
+$data = [2, 0, 1, 0];
+$solution->sortColors($data);
+var_dump($data);
