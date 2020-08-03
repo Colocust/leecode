782718f1c2c8e725f911981a92810afedc5421f3
Colocust <516100080@qq.com>
2020-08-03 14:10:31 +0800

    commit 287

--- 287.php
+++ 287.php
@@ -1,1 +1,48 @@
 <?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @return Integer
+   */
+  function findDuplicate($nums) {
+//    $left = 1;
+//    $right = count($nums) - 1;
+//    $result = -1;
+//    while ($left <= $right) {
+//      $middle = (int)(($left + $right) / 2);
+//
+//      $cnt = 0;
+//      for ($i = 0; $i < count($nums); $i++) {
+//        if ($nums[$i] <= $middle) $cnt++;
+//      }
+//
+//      if ($cnt <= $middle) $left++;
+//      else {
+//        $right--;
+//        $result = $middle;
+//      }
+//    }
+//    return $result;
+
+    $slow = $nums[0];
+    $fast = $nums[$nums[0]];
+
+    while ($slow !== $fast) {
+      $slow = $nums[$slow];
+      $fast = $nums[$nums[$fast]];
+    }
+    $slow = 0;
+
+    while ($slow !== $fast) {
+      $fast = $nums[$fast];
+      $slow = $nums[$slow];
+    }
+    return $slow;
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->findDuplicate([3, 1, 3, 4, 2]));
+
