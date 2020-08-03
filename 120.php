78bd123879a38d0d8c8016228fc29b17a6486381
Colocust <516100080@qq.com>
2020-08-03 22:21:56 +0800

    commit 120

---
+++ 120.php
@@ -0,0 +1,23 @@
+<?php
+class Solution {
+
+  /**
+   * @param Integer[][] $triangle
+   * @return Integer
+   */
+  function minimumTotal($triangle) {
+    $data = [];
+    foreach ($triangle[count($triangle) - 1] as $key => $v) {
+      $data[count($triangle) - 1][$key] = $v;
+    }
+
+    for ($i = count($triangle) - 2; $i >= 0; $i--) {
+      $item = $triangle[$i];
+      foreach ($item as $k1 => $v1) {
+        $min = min($data[$i + 1][$k1], $data[$i + 1][$k1 + 1]);
+        $data[$i][$k1] = $v1 + $min;
+      }
+    }
+    return $data[0][0];
+  }
+}
--- 64.php
+++ 64.php
@@ -1,1 +1,31 @@
 <?php
+
+class Solution {
+
+  /**
+   * @param Integer[][] $grid
+   * @return Integer
+   */
+  function minPathSum($grid) {
+    $data = [];
+
+    for ($i = count($grid) - 1; $i >= 0; $i--) {
+      for ($j = count($grid[$i]) - 1; $j >= 0; $j--) {
+        if ($j == count($grid[$i]) - 1 && $i == count($grid) - 1) {
+          $data[$i][$j] = $grid[$i][$j];
+          continue;
+        }
+        if ($j == count($grid[$i]) - 1) {
+          $data[$i][$j] = $grid[$i][$j] + $data[$i + 1][$j];
+          continue;
+        }
+        if ($i == count($grid) - 1) {
+          $data[$i][$j] = $grid[$i][$j] + $data[$i][$j + 1];
+          continue;
+        }
+        $data[$i][$j] = $grid[$i][$j] + min($data[$i + 1][$j], $data[$i][$j + 1]);
+      }
+    }
+    return $data[0][0];
+  }
+}
