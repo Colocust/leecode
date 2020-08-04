0e7b9a5e220ea92eca48991b432f49943f2bc95a
Colocust <516100080@qq.com>
2020-08-04 18:29:46 +0800

    commit 42

---
+++ 42.php
@@ -0,0 +1,29 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @return Integer
+   */
+  function maxSubArray($nums) {
+    $dp = [];
+    $length = count($nums);
+
+    $dp[$length - 1] = $nums[$length - 1];
+    $max = $dp[$length - 1];
+
+    for ($i = $length - 2; $i >= 0; $i--) {
+      $append = 0;
+      if ($dp[$i + 1] > 0) {
+        $append = $dp[$i + 1];
+      }
+      $dp[$i] = $nums[$i] + $append;
+      $max = max($max, $dp[$i]);
+    }
+    return $max;
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
