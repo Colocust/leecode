3de280d8dc110f8ee8486ed8822a30e479f94a0d
Colocust <516100080@qq.com>
2020-08-04 22:59:09 +0800

    commit 17.16

---
+++ 17.16.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @return Integer
+   */
+  function massage($nums) {
+    if (empty($nums)) return 0;
+    $length = count($nums);
+    if ($length == 1) return $nums[0];
+
+    $dp = [];
+    $dp[$length - 1] = $nums[$length - 1];
+
+    for ($i = $length - 2; $i >= 0; $i--) {
+      $dp[$i] = 0;
+
+      for ($j = $i; $j < $length; $j++) {
+        $append = 0;
+        if ($j + 2 < $length) {
+          $append = $dp[$j + 2];
+        }
+        $dp[$i] = max($dp[$i], $append + $nums[$j]);
+      }
+    }
+    return $dp[0];
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->massage([1, 2,3,1]));
