f13c71eb7647a5c5f1f17509767ce63aaef2768d
Colocust <516100080@qq.com>
2020-08-06 15:19:48 +0800

    动态规划 买股票公式

--- 121.php
+++ 121.php
@@ -7,15 +7,21 @@ class Solution {
    * @return Integer
    */
   function maxProfit($prices) {
-    //记录i之前的最小值以及最大收益 比较
-    $min = $prices[0];
-    $max = 0;
+    if (empty($prices)) return 0;
+    $dp = [];
+
+    $dp[0][0] = 0;
+    $dp[0][1] = -$prices[0];
 
     for ($i = 1; $i < count($prices); $i++) {
-      $max = max($max, $prices[$i] - $min);
-      $min = min($min, $prices[$i]);
+      $dp[$i][0] = max($dp[$i - 1][0], $dp[$i - 1][1] + $prices[$i]);
+      $dp[$i][1] = max($dp[$i - 1][1], -$prices[$i]);
     }
-
-    return $max;
+    return $dp[count($prices) - 1][0];
   }
 }
+
+//$dp[$i][$k][0] = max($dp[$i - 1][$k][0], $dp[$i - 1][$k][1] + $prices[$i]);
+//$dp[$i][$k][1] = max($dp[$i - 1][$k][1], $dp[$i - 1][$k - 1][0] - $prices[$i]);
+$solution = new Solution();
+var_dump($solution->maxProfit([1, 3, 2, 8, 4, 9]));
