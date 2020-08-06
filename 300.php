647cdb47139ecc151af30caf30fd21a6110a728a
Colocust <516100080@qq.com>
2020-08-06 20:24:40 +0800

    commit 300

---
+++ 300.php
@@ -0,0 +1,28 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @return Integer
+   */
+  function lengthOfLIS($nums) {
+    $length = count($nums);
+    if ($length == 0) return 0;
+    $dp = [];
+
+    for ($i = 0; $i < $length; $i++) $dp[$i] = 1;
+
+    for ($i = 1; $i < $length; $i++) {
+      for ($j = 0; $j < $i; $j++) {
+        if ($nums[$i] > $nums[$j]) {
+          $dp[$i] = max($dp[$i], $dp[$j] + 1);
+        }
+      }
+    }
+    return max($dp);
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
