0fa0d6613ae9f165d43d57f83f3cf11d26b9d3ef
Colocust <516100080@qq.com>
2020-08-06 20:09:41 +0800

    股票问题

---
+++ 123.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $prices
+   * @return Integer
+   */
+  function maxProfit($prices) {
+    if (empty($prices)) return 0;
+    $dp = [];
+
+    $max = 2;
+
+    for ($i = 0; $i < count($prices); $i++) {
+      $dp[$i][0][0] = 0;
+      for ($k = $max; $k >= 1; $k--) {
+        if ($i == 0) {
+          $dp[$i][$k][0] = 0;
+          $dp[$i][$k][1] = -$prices[$i];
+          continue;
+        }
+        $dp[$i][$k][0] = max($dp[$i - 1][$k][0], $dp[$i - 1][$k][1] + $prices[$i]);
+        $dp[$i][$k][1] = max($dp[$i - 1][$k][1], $dp[$i - 1][$k - 1][0] - $prices[$i]);
+      }
+    }
+
+    return $dp[count($prices) - 1][$max][0];
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->maxProfit([3, 3, 5, 0, 0, 3, 1, 4]));
---
+++ 124.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer $k
+   * @param Integer[] $prices
+   * @return Integer
+   */
+  function maxProfit($k, $prices) {
+    if (empty($prices)) return 0;
+    $dp = [];
+    for ($i = 0; $i < count($prices); $i++) {
+      $dp[$i][0][0] = 0;
+      for ($j = $k; $j >= 1; $j--) {
+        if ($i == 0) {
+          $dp[$i][$j][0] = 0;
+          $dp[$i][$j][1] = -$prices[$i];
+          continue;
+        }
+        $dp[$i][$j][0] = max($dp[$i - 1][$j][0], $dp[$i - 1][$j][1] + $prices[$i]);
+        $dp[$i][$j][1] = max($dp[$i - 1][$j][1], $dp[$i - 1][$j - 1][0] - $prices[$i]);
+      }
+    }
+    return $dp[count($prices) - 1][$k][0];
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->maxProfit(2, [2, 4, 1]));
