da3e645484ad6eacd75b1c9b4d68a7827999123d
Colocust <516100080@qq.com>
2020-08-06 22:31:23 +0800

    commit 377

---
+++ 377.php
@@ -0,0 +1,28 @@
+<?php
+
+class Solution {
+
+  /**
+   * @param Integer[] $nums
+   * @param Integer $target
+   * @return Integer
+   */
+  function combinationSum4($nums, $target) {
+    $dp = [];
+
+    $dp[0] = 1;
+    for ($i = 1; $i <= $target; $i++) $dp[$i] = 0;
+
+    for ($i = 1; $i <= $target; $i++) {
+      for ($j = 0; $j < count($nums); $j++) {
+        if ($i >= $nums[$j]) {
+          $dp[$i] += $dp[$i - $nums[$j]];
+        }
+      }
+    }
+    return $dp[$target];
+  }
+}
+
+$solution = new Solution();
+var_dump($solution->combinationSum4([1, 2, 3], 4));
