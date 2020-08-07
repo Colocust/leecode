6614e3544346d8c6f189b92de1f18fddd836d072
Colocust <516100080@qq.com>
2020-08-07 11:51:52 +0800

    commit 343

---
+++ 343.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function integerBreak($n) {
+        $dp = [];
+        $dp[1] = 1;
+        for ($i = 2; $i <= $n; $i++) {
+            $dp[$i] = 1;
+            for ($j = 1; $j < $i; $j++) {
+                $dp[$i] = max($dp[$i], $j * $dp[$i - $j], $j * ($i - $j));
+            }
+        }
+        return $dp[$n];
+    }
+}
+
+$solution = new Solution();
+var_dump($solution->integerBreak(10));
+
+//36
