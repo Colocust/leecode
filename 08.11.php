de95f037690554b77aa238ef3e00047e3bfe9e5e
Colocust <516100080@qq.com>
2020-08-11 11:42:55 +0800

    commit 08.11

--- 08.11.php
+++ 08.11.php
@@ -7,21 +7,30 @@ class Solution {
      * @return Integer
      */
     function waysToChange($n) {
+        //状态： i为使用的硬币 j为需要凑的钱
+        //dp[i][j] = dp[i-1][j] + dp[i][j - way[i]]
+        //当前不使用此硬币 + 当前使用了此硬币（容量变小）
         $ways = [1, 5, 10, 25];
 
-        $dp[0] = 1;
+        $dp = [];
 
-        for ($i = 1; $i <= $n; $i++) {
-            $dp[$i] = 0;
-
-            for ($j = 0; $j < count($ways); $j++) {
-                if ($i >= $ways[$j]) {
-                    $dp[$i] = $dp[$i - $ways[$j]] + $dp[$i];
+        for ($i = 0; $i < count($ways); $i++) {
+            for ($j = 0; $j <= $n; $j++) {
+                if ($i == 0) {
+                    $dp[0][$j] = 1;
+                    continue;
+                }
+                if ($j == 0) {
+                    $dp[$i][$j] = 1;
+                    continue;
+                }
+                $dp[$i][$j] = $dp[$i - 1][$j] % 1000000007;
+                if ($ways[$i] <= $j) {
+                    $dp[$i][$j] = ($dp[$i][$j - $ways[$i]] + $dp[$i][$j]) % 1000000007;
                 }
             }
         }
-        var_dump($dp);
-        exit;
+        return $dp[3][$n];
     }
 }
 
