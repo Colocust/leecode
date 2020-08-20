1bc0765c00687f857d1e28974c2347c24fb78ad7
Colocust <516100080@qq.com>
2020-08-20 15:20:34 +0800

    优化打家劫舍2空间复杂度

--- 213.php
+++ 213.php
@@ -7,29 +7,33 @@ class Solution {
      * @return Integer
      */
     function rob($nums) {
-        if (empty($nums)) {
-            return 0;
-        }
-
         $length = count($nums);
-        if ($length == 1) {
-            return $nums[0];
-        }
+        if ($length == 0) return 0;
+        if ($length == 1) return $nums[0];
 
-        $dp1[0] = $nums[0];
-        $dp1[1] = max($nums[1], $nums[0]);
+        $dp0_0 = $nums[0];
+        $dp0_1 = max($nums[1], $nums[0]);
 
         for ($i = 2; $i < $length - 1; $i++) {
-            $dp1[$i] = max($nums[$i] + $dp1[$i - 2], $dp1[$i - 1]);
+            $max = max($dp0_0 + $nums[$i], $dp0_1);
+            $dp0_0 = $dp0_1;
+            $dp0_1 = $max;
         }
 
-        $dp2[1] = $nums[1];
-        $dp2[2] = max($nums[1], $nums[2]);
+        if ($length == 2) {
+            return $dp0_1;
+        }
+
+        $dp1_0 = $nums[1];
+        $dp1_1 = max($nums[1], $nums[2]);
+
         for ($i = 3; $i < $length; $i++) {
-            $dp2[$i] = max($nums[$i] + $dp2[$i - 2], $dp2[$i - 1]);
+            $max = max($dp1_0 + $nums[$i], $dp1_1);
+            $dp1_0 = $dp1_1;
+            $dp1_1 = $max;
         }
 
-        return max($dp1[$length - 2], $dp2[$length - 1]);
+        return max($dp1_1, $dp0_1);
     }
 }
 
