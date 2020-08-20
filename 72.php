02ea9be82404d3706dbb2dd532fb2c5f71da230a
Colocust <516100080@qq.com>
2020-08-20 15:02:19 +0800

    commit 72

---
+++ 72.php
@@ -0,0 +1,35 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $word1
+     * @param String $word2
+     * @return Integer
+     */
+    function minDistance($word1, $word2) {
+        $dp = [];
+
+        for ($i = 0; $i <= strlen($word1); $i++) {
+            $dp[$i][0] = $i;
+        }
+        for ($i = 0; $i <= strlen($word2); $i++) {
+            $dp[0][$i] = $i;
+        }
+
+        for ($i = 1; $i <= strlen($word1); $i++) {
+            for ($j = 1; $j <= strlen($word2); $j++) {
+                if ($word1[$i - 1] == $word2[$j - 1]) {
+                    $dp[$i][$j] = $dp[$i - 1][$j - 1];
+                } else {
+                    $dp[$i][$j] = min($dp[$i - 1][$j - 1] + 1, $dp[$i - 1][$j] + 1, $dp[$i][$j - 1] + 1);
+                }
+            }
+        }
+        var_dump($dp);exit;
+        return $dp[strlen($word1)][strlen($word2)];
+    }
+}
+
+$solution = new Solution();
+var_dump($solution->minDistance('horse', 'ros'));
