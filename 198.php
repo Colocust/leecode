9ef484e6074a377d7ec42da99218e4053c1a3335
Colocust <516100080@qq.com>
2020-08-20 15:13:44 +0800

    优化打家劫舍空间复杂度

--- 198.php
+++ 198.php
@@ -7,25 +7,21 @@ class Solution {
      * @return Integer
      */
     function rob($nums) {
-        if (empty($nums)) {
-            return 0;
-        }
-
         $length = count($nums);
-        if ($length == 1) {
-            return $nums[0];
-        }
-
-        $dp[0] = $nums[0];
-        $dp[1] = max($nums[0], $nums[1]);
+        if ($length == 0) return 0;
+        if ($length == 1) return $nums[0];
+        $dp_0 = $nums[0];
+        $dp_1 = max($nums[0], $nums[1]);
 
         for ($i = 2; $i < $length; $i++) {
-            $dp[$i] = max($dp[$i - 1], $dp[$i - 2] + $nums[$i]);
+            $max = max($dp_0 + $nums[$i], $dp_1);
+            $dp_0 = $dp_1;
+            $dp_1 = $max;
         }
 
-        return $dp[$length - 1];
+        return $dp_1;
     }
 }
 
 $solution = new Solution();
-var_dump($solution->rob([1, 3, 2, 4, 5, 6]));
+var_dump($solution->rob([1,2,3,1]));
