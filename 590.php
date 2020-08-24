c7e68609c022544d5d486c0674133c5015302c22
Colocust <516100080@qq.com>
2020-08-24 22:45:23 +0800

    commit 590

---
+++ 590.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Definition for a Node.
+ * class Node {
+ *     public $val = null;
+ *     public $children = null;
+ *     function __construct($val = 0) {
+ *         $this->val = $val;
+ *         $this->children = array();
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param Node $root
+     * @return integer[]
+     */
+    function postorder($root) {
+        if (!$root) return [];
+
+        $results = [];
+//
+//        foreach ($root->children as $child) {
+//            array_merge($results, $this->postorder($child));
+//        }
+//        $results[] = $root->val;
+
+        $stack[] = $root;
+
+        while ($stack) {
+            $pop = array_pop($stack);
+            $results[] = $pop->val;
+
+            foreach ($pop->children as $child) {
+                $stack[] = $child;
+            }
+        }
+
+
+        return array_reverse($results);
+    }
+}
