d91d0793ca7338e2c522a76d4f6e03c13695de0d
Colocust <516100080@qq.com>
2020-08-24 22:05:41 +0800

    commit 938

---
+++ 938.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $L
+     * @param Integer $R
+     * @return Integer
+     */
+    function rangeSumBST($root, $L, $R) {
+        if (!$root) return 0;
+
+        if ($root->val < $L) {
+            return $this->rangeSumBST($root->right, $L, $R);
+        }
+
+        if ($root->val > $L) {
+            return $this->rangeSumBST($root->left, $L, $R);
+        }
+
+        return $root->val + $this->rangeSumBST($root->right, $L, $R) + $this->rangeSumBST($root->left, $L, $R);
+    }
+}
