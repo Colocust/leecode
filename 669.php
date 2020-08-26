8b1240eb8dd800a989b231f7e05d9c6c2bae73b9
Colocust <516100080@qq.com>
2020-08-26 21:43:56 +0800

    669

---
+++ 669.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $L
+     * @param Integer $R
+     * @return TreeNode
+     */
+    function trimBST($root, $L, $R) {
+        if (!$root) return $root;
+        if ($root->val > $R) return $this->trimBST($root->left, $L, $R);
+        if ($root->val < $L) return $this->trimBST($root->right, $L, $R);
+
+        $root->left = $this->trimBST($root->left, $L, $R);
+        $root->right = $this->trimBST($root->right, $L, $R);
+        return $root;
+    }
+}
