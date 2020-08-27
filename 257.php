85a53b7a46f3dde621bc97829cedd7c30f1e079f
Colocust <516100080@qq.com>
2020-08-27 10:23:31 +0800

    257

--- 257.php
+++ 257.php
@@ -1,1 +1,37 @@
 <?php
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+    private $ans = [];
+
+    /**
+     * @param TreeNode $root
+     * @return String[]
+     */
+    function binaryTreePaths($root) {
+        $this->binaryTree($root, "");
+        return $this->ans;
+    }
+
+    function binaryTree($root, $item) {
+        $item .= $root->val;
+        if (!$root->left && !$root->right) {
+            $this->ans[] = $item;
+            return;
+        }
+
+        $item .= '->';
+
+        if ($root->left) $this->binaryTree($root->left, $item);
+        if ($root->right) $this->binaryTree($root->right, $item);
+    }
+}
