1864fad7c0a26cbf5017b6852170911ecc9464e2
Colocust <516100080@qq.com>
2020-08-31 21:49:46 +0800

    commit 235

---
+++ 235.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param TreeNode $root
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return TreeNode
+     */
+    function lowestCommonAncestor($root, $p, $q) {
+        if ($root->val > $p->val && $root->val > $q->val) {
+            return $this->lowestCommonAncestor($root->left, $p, $q);
+        }
+
+        if ($root->val < $p->val && $root->val < $q->val) {
+            return $this->lowestCommonAncestor($root->right, $p, $q);
+        }
+        return $root;
+    }
+}
