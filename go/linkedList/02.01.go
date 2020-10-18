cf7be814286a8762d598a49e7ad4f56008545383
Colocust <516100080@qq.com>
2020-10-18 23:14:38 +0800

    commit 02.01

---
+++ go/linkedList/02.01.go
@@ -0,0 +1,26 @@
+package main
+
+func removeDuplicateNodes(head *ListNode) *ListNode {
+	dummy := new(ListNode)
+	dummy.Next = head
+
+	flag := make(map[int]bool)
+
+	cur := dummy
+
+	for cur.Next != nil {
+		if _, ok := flag[cur.Next.Val]; ok {
+			cur.Next = cur.Next.Next
+		} else {
+			flag[cur.Next.Val] = true
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
