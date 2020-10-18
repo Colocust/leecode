b35a37a053fa5dcdc13e1d2c4d8f7be79c9e64c7
Colocust <516100080@qq.com>
2020-10-18 22:40:36 +0800

    commit Offer22

---
+++ go/linkedList/Offer22.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	fast := head
+	slow := head
+
+	for i := 0; i < k; i++ {
+		fast = fast.Next
+	}
+
+	for {
+		if fast == nil {
+			break
+		}
+
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return slow
+}
