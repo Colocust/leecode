24691289dbda3cf9f78725c9098a08251b253568
Colocust <516100080@qq.com>
2020-10-18 22:55:14 +0800

    commit Offer24

---
+++ go/linkedList/Offer24.go
@@ -0,0 +1,18 @@
+package main
+
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+//
+//func reverseList(head *ListNode) *ListNode {
+//	var current *ListNode
+//
+//	for head != nil {
+//		next := head.Next
+//		head.Next = current
+//		current = head
+//		head = next
+//	}
+//	return current
+//}
