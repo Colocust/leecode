49727dcf04c23513de5d5a378c50d377da441ae5
Colocust <516100080@qq.com>
2020-10-18 22:28:03 +0800

    commit 1290

--- go/linkedList/02.03.go
+++ go/linkedList/02.03.go
@@ -1,13 +1,12 @@
 package main
 
 //删除指定节点
-
-func deleteNode(node *ListNode) {
-	node.Val = node.Next.Val
-	node.Next = node.Next.Next
-}
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//
+//func deleteNode(node *ListNode) {
+//	node.Val = node.Next.Val
+//	node.Next = node.Next.Next
+//}
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
---
+++ go/linkedList/1290.go
@@ -0,0 +1,18 @@
+package main
+
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+//
+//func getDecimalValue(head *ListNode) int {
+//	result := 0
+//	for {
+//		if head == nil {
+//			break
+//		}
+//		result = (result << 1) + head.Val
+//		head = head.Next
+//	}
+//	return result
+//}
