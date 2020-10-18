717d4b912523fdad3a6892ad743a427f04315fab
Colocust <516100080@qq.com>
2020-10-18 22:52:51 +0800

    commit Offer06

---
+++ go/linkedList/Offer06.go
@@ -0,0 +1,23 @@
+package main
+
+//func reversePrint(head *ListNode) []int {
+//	result := []int{}
+//
+//	for head != nil {
+//		result = append(result, head.Val)
+//		head = head.Next
+//	}
+//
+//	i, j := 0, len(result)-1
+//	for i <= j {
+//		result[i], result[j] = result[j], result[i]
+//		i++
+//		j--
+//	}
+//	return result
+//}
+//
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
