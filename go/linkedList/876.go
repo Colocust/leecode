64359a4cb4fde1c7c5d73948f23e1eb5215aaea2
Colocust <516100080@qq.com>
2020-10-19 21:58:32 +0800

    commit 876

--- go/linkedList/876.go
+++ go/linkedList/876.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
-	$END$
-}
+//func middleNode(head *ListNode) *ListNode {
+//	fast := head
+//	slow := head
+//
+//	for {
+//		if fast == nil || fast.Next == nil{
+//			break
+//		}
+//		fast = fast.Next.Next
+//		slow = slow.Next
+//	}
+//
+//	return slow
+//}
+//
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
