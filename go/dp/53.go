0ee76e17890cfbf9a063de1d3def3dffc67040f0
Colocust <516100080@qq.com>
2020-11-09 21:45:35 +0800

    commit 122

--- go/dp/53.go
+++ go/dp/53.go
@@ -1,22 +1,19 @@
 package main
 
-func main() {
 
-}
-
-func maxSubArray(nums []int) int {
-	dp, maxNum := nums[0], nums[0]
-
-	for i := 1; i < len(nums); i++ {
-		dp = GetMax(nums[i], dp+nums[i])
-		maxNum = GetMax(maxNum, dp)
-	}
-	return maxNum
-}
-
-func GetMax(x int, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
+//func maxSubArray(nums []int) int {
+//	dp, maxNum := nums[0], nums[0]
+//
+//	for i := 1; i < len(nums); i++ {
+//		dp = GetMax(nums[i], dp+nums[i])
+//		maxNum = GetMax(maxNum, dp)
+//	}
+//	return maxNum
+//}
+//
+//func GetMax(x int, y int) int {
+//	if x > y {
+//		return x
+//	}
+//	return y
+//}
