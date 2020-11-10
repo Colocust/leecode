0625a790216e8dc4f9a61f84cd9af6d56fa8851a
Colocust <516100080@qq.com>
2020-11-10 21:33:21 +0800

    commit 123

--- go/dp/122.go
+++ go/dp/122.go
@@ -1,23 +1,23 @@
 package main
-
-func max(x int, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func maxProfit(prices []int) int {
-	length := len(prices)
-	if length == 0 {
-		return 0
-	}
-
-	dp := make([][]int, length)
-	dp[0] = []int{0, -prices[0]}
-	for i := 1; i < length; i++ {
-		dp[i] = []int{max(dp[i-1][0], dp[i-1][1]+prices[i]), max(dp[i-1][1], dp[i-1][0]-prices[i])}
-	}
-
-	return max(dp[length-1][0], dp[length-1][1])
-}
+//
+//func max(x int, y int) int {
+//	if x > y {
+//		return x
+//	}
+//	return y
+//}
+//
+//func maxProfit(prices []int) int {
+//	length := len(prices)
+//	if length == 0 {
+//		return 0
+//	}
+//
+//	dp := make([][]int, length)
+//	dp[0] = []int{0, -prices[0]}
+//	for i := 1; i < length; i++ {
+//		dp[i] = []int{max(dp[i-1][0], dp[i-1][1]+prices[i]), max(dp[i-1][1], dp[i-1][0]-prices[i])}
+//	}
+//
+//	return max(dp[length-1][0], dp[length-1][1])
+//}
---
+++ go/dp/123.go
@@ -0,0 +1,29 @@
+package main
+
+//func maxProfit(prices []int) int {
+//	length := len(prices)
+//	if length == 0 {
+//		return 0
+//	}
+//
+//	dp := make([][][]int, length)
+//
+//	for i := 0; i < length; i++ {
+//		for k := 2; k >= 1; k-- {
+//			if i == 0 {
+//				dp[i][k] = []int{0, -prices[0]}
+//			} else {
+//
+//			}
+//		}
+//	}
+//
+//	return max(dp[length-1][2][0], dp[length-1][2][1])
+//}
+//
+//func max(x int, y int) int {
+//	if x > y {
+//		return x
+//	}
+//	return y
+//}
