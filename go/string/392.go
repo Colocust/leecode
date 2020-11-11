187ed0be135fefc1ee4f37abb2ec5f4316a1308b
Colocust <516100080@qq.com>
2020-11-11 21:48:57 +0800

    commit 392

--- go/string/392.go
+++ go/string/392.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
-	$END$
+func isSubsequence(s string, t string) bool {
+	ls, lt := len(s), len(t)
+	if ls == 0 {
+		return true
+	}
+
+	is, it := 0, 0
+	for it < lt {
+		if s[is] == t[it] {
+			is++
+		}
+		if is == ls {
+			return true
+		}
+		it++
+	}
+	return false
 }
