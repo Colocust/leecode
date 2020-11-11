50a9e092f95e0587497be050098a6b85244b5c95
Colocust <516100080@qq.com>
2020-11-11 22:00:13 +0800

    commit 746

--- go/dp/746.go
+++ go/dp/746.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
-	$END$
+func minCostClimbingStairs(cost []int) int {
+	length := len(cost)
+	if length == 0 {
+		return 0
+	}
+	if length == 1 {
+		return cost[0]
+	}
+	dp := make([]int, length+1)
+	dp[0], dp[1] = cost[0], cost[1]
+
+	for i := 2; i < length; i++ {
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+	}
+	return min(dp[length-1], dp[length-2])
 }
