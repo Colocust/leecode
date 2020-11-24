83162bc8961e13358fd6d7de9cb467792e282d72
Colocust <516100080@qq.com>
2020-11-24 21:57:11 +0800

    commit 1641

---
+++ go/dp/1641.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(countVowelStrings(1))
+}
+
+func countVowelStrings(n int) int {
+	if n == 0 {
+		return 0
+	}
+	var dp [50][5]int
+
+	for i := 0; i < n; i++ {
+		var item [5]int
+		for j := 0; j < 5; j++ {
+			var sum int
+			if i == 0 {
+				sum = 1
+			} else {
+				for k := j; k < 5; k++ {
+					sum += dp[i-1][k]
+				}
+			}
+			item[j] = sum
+		}
+		dp[i] = item
+	}
+	fmt.Println(dp)
+	return dp[n-1][0] + dp[n-1][1] + dp[n-1][2] + dp[n-1][3] + dp[n-1][4]
+}
