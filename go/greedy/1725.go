e78a2bcee4dfca61ee10dab7f794074d53892755
Colocust <516100080@qq.com>
2021-01-18 21:20:45 +0800

    commit 1725

---
+++ go/greedy/1725.go
@@ -0,0 +1,24 @@
+package main
+
+func countGoodRectangles(rectangles [][]int) int {
+	maxLen, result := 0, 0
+
+	for _, item := range rectangles {
+		length := min(item[0], item[1])
+		if length > maxLen {
+			maxLen, result = length, 1
+			continue
+		}
+		if length == maxLen {
+			result++
+		}
+	}
+	return result
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
