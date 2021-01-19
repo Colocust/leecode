f137328807b1088f53b0e0c144934a1e067bc61d
Colocust <516100080@qq.com>
2021-01-19 22:52:57 +0800

    修改394

--- php/394.php
+++ php/394.php
@@ -1,40 +1,44 @@
 <?php
 
-class Solution {
+class Solution
+{
 
-    /**
-     * @param String $s
-     * @return String
-     */
-    function decodeString($s) {
-        $stack = [];
-        $length = strlen($s);
-        for ($i = 0; $i < $length; $i++) {
-            if ($s[$i] == ']') {
-                $temp = '';
-                while (end($stack) != '[') {
-                    $temp .= array_pop($stack);
-                }
-                array_pop($stack);
+	/**
+	 * @param String $s
+	 * @return String
+	 */
+	function decodeString (string $s): string
+	{
+		$stack = [];
+		$len = strlen($s);
+		for ($i = 0; $i < $len; $i++) {
+			if ($s[$i] == ']') {
+				$temp = '';
+				while (end($stack) != '[') {
+					$temp .= array_pop($stack);
+				}
+				array_pop($stack);
 
-                $time = '';
-                while (is_numeric(end($stack))) {
-                    $time .= array_pop($stack);
-                }
-                $time = (int)(strrev($time));
-                while ($time) {
-                    for ($j = strlen($temp) - 1; $j >= 0; $j--) {
-                        $stack[] = $temp[$j];
-                    }
-                    $time--;
-                }
-            } else {
-                $stack[] = $s[$i];
-            }
-        }
-        return implode('', $stack);
-    }
+				$num = '';
+				while (is_numeric(end($stack))) {
+					$num .= array_pop($stack);
+				}
+				$num = (int)strrev($num);
+				while ($num) {
+					$tempLen = strlen($temp);
+					for ($j = $tempLen - 1; $j >= 0; $j--) {
+						array_push($stack, $temp[$j]);
+					}
+					$num--;
+				}
+
+			} else {
+				array_push($stack, $s[$i]);
+			}
+		}
+		return implode('', $stack);
+	}
 }
 
 $solution = new Solution();
-var_dump($solution->decodeString('abc3[cd]xyz'));
+var_dump($solution->decodeString('100[leetcode]'));
