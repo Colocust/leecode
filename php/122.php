c24da2233811cda120b9c6163f17709662fc15be
Colocust <516100080@qq.com>
2021-03-29 14:34:50 +0800

    commit 122

--- php/122.php
+++ php/122.php
@@ -14,7 +14,7 @@ class Solution {
         $dp[0][1] = -$prices[0];
         for ($i = 1; $i < count($prices); $i++) {
             $dp[$i][0] = max($dp[$i - 1][0], $dp[$i - 1][1] + $prices[$i]);
-            $dp[$i][1] = max($dp[$i - 1][1], $dp[$i - 2][0] - $prices[$i]);
+            $dp[$i][1] = max($dp[$i - 1][1], $dp[$i - 1][0] - $prices[$i]);
         }
         return $dp[count($prices) - 1][0];
     }
